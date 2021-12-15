8b927bd0a3ae3d61884283bd883494efb517b11d
chall6753 <85001130+chall6753@users.noreply.github.com>
2021-12-15 16:40:35 -0700

    change sql connection data

--- index.php
+++ index.php
@@ -13,18 +13,8 @@
     <h1>SENSOR DATA</h1>
     <button id='relay1' type='button' value='on'>on/off</button>
 <?php
-    // include_once 'database.php';
-     //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-// Check connection
+include_once 'database.php';
+     
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
--- src/database.php
+++ src/database.php
@@ -9,13 +9,12 @@
 		
 
     //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+
+$cleardb_server = "us-cdbr-east-05.cleardb.net";
+$cleardb_username = "b91483d47a3214";
+$cleardb_password = "b7b270b2";
+$cleardb_db = "heroku_bb2becdba3767a9";
+
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
