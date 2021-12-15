d505827961754b2116d8d985bea75bc6856b8def
chall6753 <85001130+chall6753@users.noreply.github.com>
2021-12-15 15:38:08 -0700

    change connection info

--- index.php
+++ index.php
@@ -13,8 +13,17 @@
     <h1>SENSOR DATA</h1>
     <button id='relay1' type='button' value='on'>on/off</button>
 <?php
-    include_once 'database.php';
-    
+    // include_once 'database.php';
+     //Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
